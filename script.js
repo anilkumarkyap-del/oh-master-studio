545932ee7811aa61cfbe354561baa8d5d573a3f8
anilkumarkyap-del <anilkumarky.ap@gmail.com>
2026-09-20 11:29:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const departments = document.querySelectorAll(".dept");
 
+departments.forEach(button => {
+  button.addEventListener("click", () => {
+
+    // Remove previous blue button
+    departments.forEach(b => b.classList.remove("active"));
+
+    // Make selected button blue
+    button.classList.add("active");
+
+    // Change the heading
+    document.querySelector(".panel h2").innerText =
+      "Department : " + button.innerText;
+
+  });
+});
